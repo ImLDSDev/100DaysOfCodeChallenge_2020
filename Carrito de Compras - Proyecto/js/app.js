0ee094accda82fe86c3ce4d66476b6b8d29ac2b0
Dionisio Serrano <contacto@consultoriamx.com>
2020-06-18 23:17:17 -0500

    Commit Carrito de Compras - funciones

--- Carrito de Compras - Proyecto/js/app.js
+++ Carrito de Compras - Proyecto/js/app.js
@@ -1,6 +1,7 @@
 //Variables
 const   carrito = document.getElementById('carrito'),
-        cursos  = document.getElementById('lista-cursos');
+        cursos  = document.getElementById('lista-cursos'),
+        listaCursos = document.querySelector('#lista-carrito tbody');
 
 
 
@@ -17,7 +18,7 @@ function comprarCurso(e){
     e.preventDefault();
     //Delegation para agregar-carrito
     if(e.target.classList.contains('agregar-carrito')) {
-        const curso = e.target.preventElement.parentElement;
+        const curso = e.target.parentElement.parentElement;
         //Enviamos el curso seleccionado para leer los datos.
         leerDatosCurso(curso);
     }
@@ -26,5 +27,29 @@ function comprarCurso(e){
 //Lee los datos del curso
 
 function leerDatosCurso(curso) {
-    
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id:     curso.querySelector('a').getAttribute('data-id')
+    }
+
+    insertarCarrito(infoCurso);
+
+}
+
+//Muestra el curso seleccionado en el carrito
+function insertarCarrito(curso){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${curso.imagen}">
+        </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>
+            <a href='#' class="borrar-curso" data-id="${curso.id}">X</a>
+        </td>
+    `;
+    listaCursos.appendChild(row);
 }
