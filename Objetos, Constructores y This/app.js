9a7b71e586708314d8f789a3b3b26221f4390551
Dionisio Serrano <contacto@consultoriamx.com>
2020-06-28 23:58:15 -0500

    Objetos y constructores

--- Objetos, Constructores y This/app.js
+++ Objetos, Constructores y This/app.js
@@ -1,12 +1,40 @@
 // Object Literal
 
-const cliente = {
-    nombre  : 'Juan',
-    saldo   : 200,
-    tipoCliente : function() {
+// const cliente = {
+//     nombre  : 'Juan',
+//     saldo   : 200,
+//     tipoCliente : function() {
+//         let tipo;
+        
+//         if (this.saldo > 1000) {
+//             tipo = 'Gold';
+//         } else if(this.saldo > 500) {
+//             tipo = 'Platinum';
+//         } else {
+//             tipo = 'Normal';
+//         }
+
+//         return tipo;
+//     }
+// }
+
+// console.log(cliente.tipoCliente() );
+
+// console.log(cliente.saldo );
+
+
+
+//Metodo Alternativo
+
+function Cliente(nombre, saldo) {
+    this.nombre  = nombre,
+    this.saldo   = saldo,
+    this.tipoCliente = function() {
         let tipo;
         
         if (this.saldo > 1000) {
+            tipo = 'Gold';
+        } else if(this.saldo > 500) {
             tipo = 'Platinum';
         } else {
             tipo = 'Normal';
@@ -16,4 +44,7 @@ const cliente = {
     }
 }
 
-console.log(cliente.tipoCliente() );
+const persona = new Cliente('Pedro', 20000);
+console.log(persona);
+const persona2 = new Cliente('Pedro2', 400);
+console.log(persona2.tipoCliente() );
