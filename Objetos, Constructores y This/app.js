ee45bc9cc51923671142e29961e38c0848327e8d
Dionisio Serrano <contacto@consultoriamx.com>
2020-06-30 23:54:59 -0500

    Prototypes

--- Objetos, Constructores y This/app.js
+++ Objetos, Constructores y This/app.js
@@ -26,25 +26,60 @@
 
 //Metodo Alternativo
 
-function Cliente(nombre, saldo) {
-    this.nombre  = nombre,
-    this.saldo   = saldo,
-    this.tipoCliente = function() {
-        let tipo;
+// function Cliente(nombre, saldo) {
+//     this.nombre  = nombre,
+//     this.saldo   = saldo,
+//     this.tipoCliente = function() {
+//         let tipo;
         
-        if (this.saldo > 1000) {
-            tipo = 'Gold';
-        } else if(this.saldo > 500) {
-            tipo = 'Platinum';
-        } else {
-            tipo = 'Normal';
-        }
+//         if (this.saldo > 1000) {
+//             tipo = 'Gold';
+//         } else if(this.saldo > 500) {
+//             tipo = 'Platinum';
+//         } else {
+//             tipo = 'Normal';
+//         }
 
-        return tipo;
-    }
+//         return tipo;
+//     }
+// }
+
+// const persona = new Cliente('Pedro', 20000);
+// console.log(persona);
+// const persona2 = new Cliente('Pedro2', 400);
+// console.log(persona2.tipoCliente() );
+
+//Prototypes
+function Cliente(nombre, saldo){
+    this.nombre = nombre;
+    this.saldo = saldo;
 }
 
-const persona = new Cliente('Pedro', 20000);
-console.log(persona);
-const persona2 = new Cliente('Pedro2', 400);
-console.log(persona2.tipoCliente() );
+Cliente.prototype.tipoCliente = function() {
+    let saldo;
+    if(this.saldo > 1000) {
+        tipo = 'Gold';
+    } else if (this.saldo > 500) {
+        tipo = 'Platinum';
+    } else {
+        tipo = 'Normal';
+    }
+    return tipo;
+}
+
+Cliente.prototype.nombreClienteSaldo = function() {
+    return `Nombre: ${this.nombre}, Tu saldo es de ${this.saldo}, Tipo Cliente ${this.tipoCliente() }`;
+}
+
+//Retirar Saldo
+Cliente.prototype.retirarSaldo = function(retiro) {
+    return this.saldo -= retiro;
+}
+
+const cliente1 = new Cliente('Pedro', 100);
+
+console.log(cliente1.tipoCliente() );
+console.log(cliente1.nombreClienteSaldo() );
+
+cliente1.retirarSaldo(300);
+console.log(cliente1.nombreClienteSaldo() );
