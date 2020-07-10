b49cc0804294265d23c1ba75915ad79d2b763b2e
Dionisio Serrano <contacto@consultoriamx.com>
2020-07-09 23:58:26 -0500

    Prototype Cotizar Seguro

--- Cotizador de seguro de auto/js/app.js
+++ Cotizador de seguro de auto/js/app.js
@@ -6,6 +6,43 @@ function Seguro(marca, anio, tipo) {
     this.tipo = tipo;
 }
 
+Seguro.prototype.cotizarSeguro = function(informacion) {
+    /*
+        1 = americano 1.15
+        2 = asiatico 1. 05
+        3 = europeo 1.35
+    */
+   let cantidad;
+   const base = 2000;
+
+   switch(this.marca) {
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+   }
+
+   //Leer el año   
+   const diferencia = new Date().getFullYear() - this.anio;
+   cantidad -= ((diferencia * 3) * cantidad) / 100;
+   /*
+        Seguro basico mas 30%
+        Seguro Completo mas 50%
+   */
+  if(this.tipo === 'basico') {
+      cantidad *= 1.30;
+  } else {
+      cantidad *= 1.50;
+  }
+
+  return cantidad;
+}
+
 //Todo lo que se muestra
 function Interfaz() {
 
@@ -57,7 +94,10 @@ formulario.addEventListener('submit', function(e) {
         interfaz.mostrarError('Faltan Datos, revisa el formulario y prueba de nuevo', 'error');
         
     } else {
-        interfaz.mostrarError('Todo Correcto');
+        const seguro = new Seguro(marcaSeleccionada, anioSeleccioando, tipo);
+
+        //Cotizar el seguro
+        const cantidad = seguro.cotizarSeguro(seguro);
     }
 
 });
